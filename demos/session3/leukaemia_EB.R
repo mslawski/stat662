e7e01fa2ad5c4f02b1321fcc3ed965107c4a1c6a
mslawski <56366391+mslawski@users.noreply.github.com>
2023-02-06 21:33:27 -0500

    Update leukaemia_EB.R

--- demos/session3/leukaemia_EB.R
+++ demos/session3/leukaemia_EB.R
@@ -38,37 +38,3 @@ plot(ecdf(rowMeans(leuk[,1:45]) -  rowMeans(leuk[,46:72])))
 
 
 
-# all rows are standardized!
-C <- tcrossprod(as.matrix(leuk))/(n-1)
-Dsq <- 2*(1 - C)
-
-Doff <- sqrt(Dsq[lower.tri(D, diag = FALSE)])
-
-boxplot(Doff)
-
-### assess asymptotic normality
-
-Drows <- dist(as.matrix(t(leuk)), method = "euclidean")
-
-Drows_matrix <- matrix(nrow = n, ncol = n, data = 0)
-Drows_matrix[lower.tri(Drows_matrix)] <- Drows
-
-for(i in 1:n)
-    for(j in i:n)
-        Drows_matrix[i,j] <- Drows_matrix[j,i]
-#for(i in 1:(nrow(Drows_matrix)-1))
-#    for(j in ((i+1):min((i+1), nrow(Drows_matrix))))
-#        Drows_matrix[i,j] <- Drows_matrix[j,i]
-
-boxplot(Drows_matrix[2:n,1])
-ix <- 10#5
-boxplot(Drows_matrix[setdiff(1:n,ix),ix])
-
-ix1 <- 13
-ix2 <- 65
-plot(Drows_matrix[setdiff(1:n,ix1),ix1], Drows_matrix[setdiff(1:n,ix2),ix2])
-
-
-#pdf("../fig/heatmap_leuk.pdf")
-#heatmap(as.matrix(leuk))
-#dev.off()
