e5713d102bca338bd25d65542ca4d9fcc0447cd3
mslawski <56366391+mslawski@users.noreply.github.com>
2024-01-22 23:04:33 -0500

    Update quiz2_solution.R

--- quizzes/quiz2_solution.R
+++ quizzes/quiz2_solution.R
@@ -1,1 +1,13 @@
-
+rho=0.3
+d = 10^6
+z<-rnorm(d)
+u1<-rnorm(d)
+u2<-rnorm(d)
+u3<-rnorm(d)
+x1=sqrt(rho)*z+sqrt(1-rho)*u1
+x2=sqrt(rho)*z+sqrt(1-rho)*u2
+x3=sqrt(rho)*z+sqrt(1-rho)*u3
+data <- data.frame(x1,x2,x3)
+plot(x1,x2)
+plot(x1,x3)
+plot(x2,x3)
